eca420f9c9702ab5f1897815cd1bfe3aac0aa78a
hacragajeuddou-5964@toolbox.ovh <hacragajeuddou-5964@toolbox.ovh>
2023-07-10 12:32:42 +0000

    Recipe UIPOC.compute_Output saved through notebook notebook editor for compute_Output

--- recipes/compute_Output.py
+++ recipes/compute_Output.py
@@ -1,28 +1,23 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 input = dataiku.Dataset("Processed_Input")
-input_df = Processed_Input.get_dataframe()
+input_df = input.get_dataframe()
 ghg_emmissions = dataiku.Dataset("GHG_Emissions_processed")
-ghg_emmissions_df = GHG_Emissions_processed.get_dataframe()
+ghg_emmissions_df = ghg_emmissions.get_dataframe()
 
-
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 labels = pd.Series(list(range(2020,2100)))
 cash = input_df["Cash and cash equivalents"]
 
-pd.
+Output_df = pd.concat(labels, cash)
 
-
-Output_df = ... # Compute a Pandas dataframe to write into Output
-
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 Output = dataiku.Dataset("Output")
 Output.write_with_schema(Output_df)
