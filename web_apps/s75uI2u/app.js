73e01829f02fc1eb5bb127687fbf0f213449d35d
hacragajeuddou-5964@toolbox.ovh <hacragajeuddou-5964@toolbox.ovh>
2023-07-10 09:39:28 +0000

    Saved web app ScenarioExplorerInput (UIPOC.s75uI2u)

--- web_apps/s75uI2u/app.js
+++ web_apps/s75uI2u/app.js
@@ -12,7 +12,7 @@ function getData(form) {
 document.getElementById("mainForm").addEventListener("submit", function (e) {
     alert("Show me");
     console.log(e);
-    console.log(e.target);
+    console.log(serialize(document.forms[0]););
   e.preventDefault();
   getData(e.target);
 });
