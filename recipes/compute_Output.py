137aa168ff607951d2823fe5b6016371f8d82a6f
hacragajeuddou-5964@toolbox.ovh <hacragajeuddou-5964@toolbox.ovh>
2023-07-10 13:45:04 +0000

    Recipe UIPOC.compute_Output saved through notebook notebook editor for compute_Output_3

--- recipes/compute_Output.py
+++ recipes/compute_Output.py
@@ -1,13 +1,38 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
+# # Processing
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
+# ## Imports
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+
+# Write recipe outputs
+Output = dataiku.Dataset("Output")
+Output.write_with_schema(Output_df)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
+# ## Reading Data
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
-GHG_Emissions_prepared = dataiku.Dataset("GHG_Emissions_prepared")
-GHG_Emissions_prepared_df = GHG_Emissions_prepared.get_dataframe()
-Input_prepared = dataiku.Dataset("Input_prepared")
-Input_prepared_df = Input_prepared.get_dataframe()
+ghg_input = dataiku.Dataset("GHG_Emissions_prepared")
+ghg_input_df = ghg_input.get_dataframe()
+finance_input = dataiku.Dataset("Input_prepared")
+finance_input_df = finance_input.get_dataframe()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
+# # Compute Logic
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+years = pd.Series(list(range(2021,2100)))
+revenue = finance_input[]
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 
 
 # Compute recipe outputs
@@ -15,8 +40,3 @@ Input_prepared_df = Input_prepared.get_dataframe()
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
 
 Output_df = ... # Compute a Pandas dataframe to write into Output
-
-
-# Write recipe outputs
-Output = dataiku.Dataset("Output")
-Output.write_with_schema(Output_df)
