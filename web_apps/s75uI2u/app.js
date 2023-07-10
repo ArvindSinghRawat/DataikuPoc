1703b6395651fc638b9bf2d03b393b3e965a5c3f
hacragajeuddou-5964@toolbox.ovh <hacragajeuddou-5964@toolbox.ovh>
2023-07-10 09:45:46 +0000

    Saved web app ScenarioExplorerInput (UIPOC.s75uI2u)

--- web_apps/s75uI2u/app.js
+++ web_apps/s75uI2u/app.js
@@ -1,13 +1,37 @@
 function getData(form) {
   var formData = new FormData(form);
-    console.log(formData);
-
-  for (var pair of formData.entries()) {
-    console.log(pair[0] + ": " + pair[1]);
-  }
+    
+  console.log(obj2FormData(formData);
 
   console.log(Object.fromEntries(formData));
 }
+
+function obj2FormData(obj, formData = new FormData()){
+
+    this.formData = formData;
+
+    this.createFormData = function(obj, subKeyStr = ''){
+        for(let i in obj){
+            let value          = obj[i];
+            let subKeyStrTrans = subKeyStr ? subKeyStr + '[' + i + ']' : i;
+
+            if(typeof(value) === 'string' || typeof(value) === 'number'){
+
+                this.formData.append(subKeyStrTrans, value);
+
+            } else if(typeof(value) === 'object'){
+
+                this.createFormData(value, subKeyStrTrans);
+
+            }
+        }
+    }
+
+    this.createFormData(obj);
+
+    return this.formData;
+}
+
 
 document.getElementById("mainForm").addEventListener("submit", function (e) {
     alert("Show me");
